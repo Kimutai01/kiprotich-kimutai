173c2a0cbc9bab62c2ab7bebef00acfea49e5ba1
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-02-08 06:44:39 +0300

    Added technologies

--- components/Skillset.jsx
+++ components/Skillset.jsx
@@ -6,6 +6,7 @@ import { CiDatabase } from "react-icons/ci";
 import { BsPen } from "react-icons/bs";
 import { MdOutlineDesignServices } from "react-icons/md";
 import { IoMdCodeWorking } from "react-icons/io";
+import Image from "next/image";
 
 const Skillset = () => {
   return (
@@ -23,11 +24,11 @@ const Skillset = () => {
           </p>
         </div>
         <div className="mr-8">
-          <div>
+          <div className="group">
             <MdOutlineDesignServices
               size={60}
               color="#4c24dd"
-              className="hover:scale-110  duration-300"
+              className="group-hover:scale-110  duration-300"
             />
             <h2 className="text-[#fff] mt-2 text-2xl font-bold">
               Frontend Developer
@@ -41,11 +42,11 @@ const Skillset = () => {
               HTML, CSS, JavaScript, React, Nextjs, and Tailwind.
             </p>
           </div>
-          <div className="mt-4">
+          <div className="mt-4 group">
             <IoMdCodeWorking
               size={60}
               color="#4c24dd"
-              className="hover:scale-110  duration-300"
+              className="group-hover:scale-110  duration-300"
             />
             <h2 className="text-[#fff] mt-2 text-2xl font-bold">
               Backend Developer
@@ -61,11 +62,11 @@ const Skillset = () => {
           </div>
         </div>
         <div>
-          <div>
+          <div className="group">
             <CiDatabase
               size={60}
               color="#4c24dd"
-              className="hover:scale-110  duration-300"
+              className="group-hover:scale-110  duration-300"
             />
             <h2 className="text-[#fff] mt-2 text-2xl font-bold">
               Database design
@@ -79,11 +80,11 @@ const Skillset = () => {
               Postgresql.
             </p>
           </div>
-          <div className="mt-8">
+          <div className="mt-8 group">
             <BiPen
               size={60}
               color="#4c24dd"
-              className="hover:scale-110  duration-300"
+              className="group-hover:scale-110  duration-300"
             />
             <h2 className="text-[#fff] mt-2 text-2xl font-bold">
               Content Writing
@@ -96,6 +97,121 @@ const Skillset = () => {
               can write any content from scratch. I have experience with writing
               articles, blogs, and essays.
             </p>
+          </div>
+        </div>
+      </div>
+      <h1 className=" mt-10 mb-5 text-center text-white text-4xl text-bold">
+        Technologies
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8 p-6 md:p-16">
+        <div
+          className="p-6 hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/html.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">HTML</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6   hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/css.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">CSS</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6 hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/javascript.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">Javascript</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6  hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/react.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">REACT JS</h3>
+            </div>
+          </div>
+        </div>
+        <div
+          className="p-6  hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/ruby.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">RUBY</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6  hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/rails.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">RUBY ON RAILS</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6  hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/next.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">NEXT JS</h3>
+            </div>
+          </div>
+        </div>
+
+        <div
+          className="p-6  hover:scale-105 ease-in duration-300"
+          style={{ border: "1px solid #4c24dd", borderRadius: "10px" }}
+        >
+          <div className="grid grid-cols-2 gap-4  justify-center items-center">
+            <div className="m-auto">
+              <Image src="/tailwind.png" alt="html" width={50} height={50} />
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <h3 className="text-[#5b6876]">TAILWIND CSS</h3>
+            </div>
           </div>
         </div>
       </div>
